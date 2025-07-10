8d8b44483bdab684e6fe4d88f9a6704a524782bc
Saransh Koirala <upwhats498@gmail.com>
2025-07-10 16:22:50 +0545

    added the cart and favourite page

---
+++ app/cart/page.tsx
@@ -0,0 +1,3 @@
+export default function Cart() {
+  return <div>This section isn't designed yet:(</div>;
+}
---
+++ app/favourite/page.tsx
@@ -0,0 +1,3 @@
+export default function Favourite() {
+  return <div>This seciton isn't designed yet :(</div>;
+}
